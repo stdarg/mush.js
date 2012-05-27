a64c172097d9a0fb3f132a1e68201806a03c7d2e
Edmond Meinfelder <emeinfelder@ngmoco.com>
2012-05-27 16:40:06 -0700

    Moved createHash to its own file. Many small bug fixes exposed by assert.

---
+++ lib/MushUtils.h
@@ -0,0 +1,19 @@
+'use strict';
+
+var assert = require('assert');
+var is = require('./is');
+
+exports.createHash = function(strIn) {
+    assert.ok(strIn);
+    assert.ok(is.nonEmptyStr(strIn));
+
+    var crypto = require('crypto');
+    var hmac = crypto.createHmac('sha1', 'potrzebie');
+    hmac.update(strIn);
+
+    var hash = hmac.digest('base64');
+    assert.ok(hash);
+    assert.ok(is.nonEmptyStr(hash));
+
+    return hash;
+};
