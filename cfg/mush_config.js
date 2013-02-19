a3db0c08d91187099832141a297e32237afedb3a
Edmond Meinfelder <edmond@stdarg.com>
2013-02-18 16:59:37 -0800

    Using fuzelog and created it from config info in config file.

--- cfg/mush_config.js
+++ cfg/mush_config.js
@@ -1,10 +1,25 @@
 
 module.exports = {
-    
+
     mush : {
         version : '0.0.1',
         name : 'mush.js',
         variant : '',
+    },
+
+    logging : {
+        level: 'info',              // INFO logging level
+        name: 'mush.js',            // Category name, shows as %c in pattern
+
+        // FileStream to log to (can be file name or a stream)
+        file: __dirname + '../mush.log',
+        fileFlags: 'a'              // Flags used in fs.createWriteStream to create log file
+
+        consoleLogging: true,       // Flag to direct output to console
+        colorConsoleLogging: true,  // Flag to color output to console
+
+        // Usage of the log4js layout
+        logMessagePattern: '[%d{ISO8601}] [%p] %c - %m{1}'
     },
 
     mongodb : {
