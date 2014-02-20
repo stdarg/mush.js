2bda1cb2aba258bb77a606ec84fb6868de75d94d
Edmond Meinfelder <edmond@stdarg.com>
2014-02-20 13:24:53 -0800

    Commented out unfinished dig cmd.

--- src/cmd_table.js
+++ src/cmd_table.js
@@ -38,7 +38,7 @@ function CommandTable() {
     this._addCmd('say',         1, Commands.prototype.say,      yesConPlayer, noDconPlayer,  shortAliases);
     this._addCmd('who',         0, Commands.prototype.who,      yesConPlayer, yesDconPlayer, shortAliases);
     this._addCmd('@describe',   0, Commands.prototype.describe, yesConPlayer, noDconPlayer,  shortAliases);
-    this._addCmd('@dig',        0, Commands.prototype.dig,      yesConPlayer, noDconPlayer,  shortAliases);
+    //this._addCmd('@dig',        0, Commands.prototype.dig,      yesConPlayer, noDconPlayer,  shortAliases);
 
     // additional aliases
     this._makeAlias('who',     'WHO');
